ee0cb6c7ec8862750c69d5c9c05b39da128d25b2
roman <romanbalitskij9@gmail.com>
2019-06-26 21:01:36 +0300

    create product seed image

--- database/factories/ProductTableFactory.php
+++ database/factories/ProductTableFactory.php
@@ -8,25 +8,28 @@ use Faker\Generator as Faker;
 use Illuminate\Support\Str;
 
 $factory->define(Product::class, function (Faker $faker) {
-
-    $num_originals = 9;
-    $original_files_folder = '/shop/img';
-    // randomly select an image
-    $pathToFile = $original_files_folder . random_int(1, $num_originals) . '.jpg';
-
-    $actual_price    = rand( 15.5, 100.5 );
-    $discount_factor = ( 10 / 100 ) * $actual_price;
+    $image = [
+        'product-1.jpg','product-2.jpg','product-3.jpg','product-4.jpg','product-5.jpg',
+        'product-6.jpg','product-7.jpg','product-8.jpg','product-9.jpg','product-big-1.jpg'
+    ];
+        $actual_price    = rand( 15.5, 100.5 );
+        $discount_factor = ( 10 / 100 ) * $actual_price;
     return [
 
-        'name' => $name =  $faker->realText(40),
-        'slug'  => Str::slug($name),
-        'description' => $faker->realText(200),
-        'featured'  => true,
-        'image' => $pathToFile,
+        'name'       => $name =  $faker->realText(40),
+        'slug'       => Str::slug($name),
+        'description'=> $faker->realText(200),
+        'featured'   => true,
+        'image'      => $faker->randomElement($image),
         'original_price' => $actual_price,
         'discount_price' => $discount_factor,
-        'quantity' => $faker->numberBetween(1,10),
-        'views'  => $faker->numberBetween(1,100),
+        'quantity'   => $faker->numberBetween(1,10),
+        'views'      => $faker->numberBetween(1,100),
        // 'date ' => \Carbon\Carbon::now()
+
+
+
     ];
+
+
 });
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-       //  $this->call(CategoryTableeSeed::class);
+       // $this->call(CategoryTableeSeed::class);
         $this->call(ProductTableSeed::class);
     }
 }
